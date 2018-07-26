48b9cdd9a5e7e39d373af236585a9ea2239be83f
Rahul Raj <omrahulrajcse@gmail.com>
2018-07-26 12:14:39 +0530

    fix jar issue

--- src/main/java/com/example/demo/service/DL4JServiceImpl.java
+++ src/main/java/com/example/demo/service/DL4JServiceImpl.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
 
+
 import examples.CustomerLossPrediction;
+
+
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
